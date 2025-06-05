2d36bfb8e0d9707715199e98479c78c620b84751
connortpeterson <connorthpeterson@gmail.com>
2025-06-05 14:17:01 +0000

    Fix build: move index.css and resolve import path

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -10,4 +9,4 @@ export default defineConfig({
   server: {
     historyApiFallback: true
   }
-})
+});
